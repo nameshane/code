a534779f62852eb3573ad5481bc00a3c5171df19
Administrator <Administrator@50-PC>
2017-11-13 14:05:18 +0800

    http 封装

---
+++ src/main/java/com/nameshane/http/Method.java
@@ -0,0 +1,6 @@
+package com.nameshane.http;
+
+public enum Method {
+	GET,POST,DELETE,PUT;
+	//put：更新或创建
+}
---
+++ src/main/java/com/nameshane/http/Status.java
@@ -0,0 +1,10 @@
+package com.nameshane.http;
+
+public interface Status {
+	/**
+	 * HTTP Status-Code 200: OK.
+	 */
+	public static final int HTTP_OK=200;
+	
+	public static final int HTTP_NOT_FOUND = 404;
+}
